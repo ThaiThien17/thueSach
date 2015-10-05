5a63323340da8330233c783d15c0fddbc51a6fc7
ThaiThien17 <thaithienars@gmail.com>
2015-10-05 22:20:38 +0700

    Update
    Cập nhập

--- build/classes/InterfaceLayer/java/MasterController_UI.rs
+++
@@ -1,1 +0,0 @@
-InterfaceLayer.MasterController_UI
--- src/InterfaceLayer/java/MasterController_UI.java
+++ src/InterfaceLayer/java/MasterController_UI.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package InterfaceLayer;
+package InterfaceLayer.java;
 
 import javafx.application.Application;
 import static javafx.application.Application.launch;
@@ -20,7 +20,7 @@ public class MasterController_UI extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("./DieuKhien_UI.fxml"));        
+        Parent root = FXMLLoader.load(getClass().getResource("../DieuKhien_UI.fxml"));        
         Scene scene = new Scene(root);
         
         stage.setScene(scene);
